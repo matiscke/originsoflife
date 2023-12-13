85e289b5b198a465e2344b53924d23873e4bed9c
Martin Schlecker <martin.schlecker@mailbox.org>
2023-12-12 18:32:22 -0700

    add true evidence plot

---
+++ src/scripts/semian_true_evidence.py
@@ -0,0 +1,131 @@
+import paths
+import pickle
+import plotstyle
+
+plotstyle.styleplots()
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.special as scp
+from scipy import stats
+
+rng = np.random.default_rng(seed=42)
+
+def BFH1(k, theta, pi, theta_l):
+    """
+    Evidence for H1 against H0
+    k number of life detection in the sample
+    theta : vector of theta values in the sample
+    pi : probability of life emergence
+    theta_l : threshold of theta for life emergence
+
+    returns bf the bayes ratio P(k|H1)/P(k|H0)
+    """
+    if len(np.shape(theta_l)) == 2:
+        nl = np.sum(theta > theta_l, axis=1)
+    elif len(np.shape(theta_l)) == 1:
+        nl = np.sum(theta > np.reshape(theta_l, (len(theta_l), 1)), axis=1)
+    else:
+        nl = np.sum(theta > theta_l)
+    bf = stats.binom.pmf(k, nl, pi) / stats.binom.pmf(k, len(theta), pi)
+    return bf
+
+def sample_H0(theta,pi,theta_l):
+    """
+    Generates a sample of values of k under H0
+    """
+    # theta is the vector of theta values
+    # pi is the probability of life
+    # theta_l is the value of the limit theta
+    return(np.random.binomial(len(theta),pi))
+
+
+def sample_H1(theta,pi,theta_l):
+    """
+    Generates a sample of values of k under H1
+    """
+    # theta is the vector of theta values
+    # pi is the probability of life
+    # theta_l is the value of the limit theta
+    if len(np.shape(theta_l))==2:
+        X = np.random.binomial(np.sum(theta>theta_l,axis=1),pi)
+    elif len(np.shape(theta_l))==1:
+        X = np.random.binomial(np.sum(theta>np.reshape(theta_l,(len(theta_l),1)),axis=1),pi)
+    else:
+        X = np.random.binomial(np.sum(theta>theta_l),pi)
+    return(X)
+
+def get_evidence(sampsize=100):
+    # For convenience, we redefine theta here so that one can play with sample size and such
+    theta = rng.uniform(
+        0, 1, sampsize
+    )  # vector theta chosen at random for demonstrative purposes for simplicity we assume theta in 0,1
+
+    nboot = 100000  # it is advised to use larger nboot because we have increased the sampling space quite a bit (probably already too much)
+    pisamp = rng.uniform(0, 1, nboot)
+    theta_lsamp = rng.uniform(0, 1, nboot)
+    kh1samp = sample_H1(theta, pisamp, theta_lsamp)
+    kh0samp = sample_H0(theta, pisamp, theta_lsamp)
+
+    ## Calculate sample evidence
+    bfh1_kh1 = BFH1(
+        kh1samp, theta, pisamp, theta_lsamp
+    )  # evidence for H1 when H1 is true (k is sampled under H1)
+    bfh1_kh0 = BFH1(
+        kh0samp, theta, pisamp, theta_lsamp
+    )  # evidence for H1 when H0 is true (k is sampled under H0)
+    bfh0_kh0 = 1 / bfh1_kh0  # evidence for H0 when H0 is true
+
+    return bfh1_kh1, bfh0_kh0
+
+
+def true_evidence():
+    gtvec = 10 ** np.linspace(-2, 3, 300)
+    fig, ax = plt.subplots()
+    
+    for s, ls, showlabel in zip([10, 100], ["-", "--"], ["", "_"]):
+        bfh1_kh1, bfh0_kh0 = get_evidence(s)
+    
+        ax.plot(
+            gtvec,
+            [np.sum(bfh1_kh1 > g) / len(bfh1_kh1) for g in gtvec],
+            label=showlabel + "True evidence for $H_\mathrm{3}$",
+            c="C0",
+            ls=ls,
+        )
+        ax.plot(
+            gtvec,
+            [np.sum(bfh0_kh0 > g) / len(bfh1_kh1) for g in gtvec],
+            label=showlabel + "True evidence for $H_\mathrm{3, null}$",
+            c="C1",
+            ls=ls,
+        )
+    
+    # ax.vlines(x=[1],ymin=0,ymax=1,linestyles='--',colors='grey')
+    ax.vlines(x=[10], ymin=0, ymax=1, linestyles="-", lw=1, colors="gray")
+    ax.text(
+        9, 1, "strong", rotation=90, va="top", ha="right", c="k", alpha=1.0, fontsize=12
+    )
+    ax.vlines(x=[100], ymin=0, ymax=1, linestyles="-", lw=1, colors="gray")
+    ax.text(
+        94, 1, "extreme", rotation=90, va="top", ha="right", c="k", alpha=1.0, fontsize=12
+    )
+    ax.legend(
+        fontsize=12,
+        loc="lower left",
+        ncol=99,
+        bbox_to_anchor=(0.0, 0.99),
+        frameon=False,
+        columnspacing=1.6,
+    )
+    ax.set_xscale("log")
+    ax.set_ylabel("P(evidence>x)")
+    ax.set_xlabel("x")
+
+    return fig, ax
+
+fig, ax = true_evidence()
+
+
+# fig.tight_layout(
+fig.savefig(paths.figures / "semian_true_evidence.pdf")
