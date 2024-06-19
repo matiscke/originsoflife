f0a12985ff07c4b5fe430721bf71c3078a2f4e33
Martin Schlecker <martin.schlecker@mailbox.org>
2024-06-18 17:09:52 -0700

    add dashed line at threshold NUV flux

--- src/scripts/detections_uv.py
+++ src/scripts/detections_uv.py
@@ -1,17 +1,19 @@
 import dill
 import paths
 import plotstyle
+from src.scripts.utils import read_var_latex
 
 plotstyle.styleplots()
 
 import matplotlib.pyplot as plt
 
 
-def plot_detections_uv(eec, fig, ax):
+def plot_detections_uv(eec, fig, ax, NUV_thresh):
     ax.scatter(eec["max_nuv"], eec["has_O2"], s=9.0)
+    ax.axvline(x=NUV_thresh, linestyle="--", color="grey")
     ax.set_yticks([0, 1])
     # ax.set_xlim([0,10])
-    ax.set_yticklabels(["no biosignature", "biosignature"])#, fontsize=24)
+    ax.set_yticklabels(["no biosignature", "biosignature"])  # , fontsize=24)
 
     ax.spines["right"].set_visible(False)
     ax.spines["top"].set_visible(False)
@@ -21,15 +23,15 @@ def plot_detections_uv(eec, fig, ax):
     return fig, ax
 
 
-fig, axs = plt.subplots(2, 1, figsize=(15, 4.), sharex=True)
-plt.subplots_adjust(hspace = 0.9)  # Adjust to the desired value
+fig, axs = plt.subplots(2, 1, figsize=(15, 4.0), sharex=True)
+plt.subplots_adjust(hspace=0.9)  # Adjust to the desired value
 for spt, ax in zip(["FGK", "M"], axs):
     with open(paths.data / "pipeline/data_{}.dll".format(spt), "rb") as f:
         data = dill.load(f)
 
-    fig, ax = plot_detections_uv(data, fig, ax)
+    fig, ax = plot_detections_uv(data, fig, ax, NUV_thresh=read_var_latex("NUV_thresh"))
     ax.set_title(f"{spt}-type host stars")
 
-ax.set_xlabel("max. NUV irradiance [erg/s/$cm^2$]")#, fontsize=24)
+ax.set_xlabel("max. NUV irradiance [erg/s/$cm^2$]")  # , fontsize=24)
 
 fig.savefig(paths.figures / "detections_uv.pdf")
