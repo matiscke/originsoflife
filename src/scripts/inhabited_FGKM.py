5f1367b7bbcf78dc108144660aa77f23ff5ca162
Martin Schlecker <martin.schlecker@mailbox.org>
2024-06-21 12:42:07 -0700

    revise histogram plot, separate spectral types

--- src/scripts/inhabited_FGKM.py
+++ src/scripts/inhabited_FGKM.py
@@ -1,22 +1,28 @@
 import dill
+from matplotlib import pyplot as plt
+
 import paths
 import plotstyle
 plotstyle.styleplots()
 
 import seaborn as sns
 
-def plot_inhabited_FGKM(d):
+def plot_inhabited_FGKM(d, fig, ax):
     """Plot the distribution of inhabited planets in the FGKM diagram.
 
     Parameters
     ----------
     d : bioverse Table
         The table containing the sample.
+    fig : matplotlib figure
+        The figure to plot on
+    ax : matplotlib axis
+        The axis to plot on
 
     Returns
     -------
-    g : seaborn.FacetGrid
-        a seaborn FacetGrid object containing the plot.
+    ax : matplotlib axis
+        The axis containing the plot
     """
 
     dd = d.to_pandas()
@@ -32,16 +38,33 @@ def plot_inhabited_FGKM(d):
 
     dd.loc[:, "Category"] = dd.apply(cat, axis=1)
     
-    g = sns.catplot(
-        dd, x="SpT", kind="count", hue="Category", order=['F', 'G', 'K', 'M']
+    # g = sns.catplot(
+    ax = sns.countplot(
+        dd, ax=ax, x="SpT",
+        # kind="count",
+        hue="Category", order=['F', 'G', 'K', 'M']
     )  # , hue_order = [True, False])
-    g.ax.set_yscale("log")
+    # ax.set_yscale("log")
 
-    g.savefig(paths.figures / "inhabited_FGKM.pdf")
-    return g
+    return ax
 
 
-with open(paths.data / 'pipeline/sample.dll', 'rb') as f:
-    sample = dill.load(f)
+fig, axs = plt.subplots(2, 1, figsize=(5, 8), sharex=False, sharey=True, gridspec_kw={"hspace": 0.3})
+for spt, ax in zip(["FGK", "M"], axs):
+    with open(paths.data / "pipeline/sample_{}.dll".format(spt), "rb") as f:
+        sample = dill.load(f)
 
-g = plot_inhabited_FGKM(sample)
+    ax = plot_inhabited_FGKM(sample, fig, ax)
+    ax.set_title(f"{spt}-type host stars")
+
+    # add sample size to the upper right corner
+    ax.text(0.97, 0.9, f"N = {len(sample)}", transform=ax.transAxes, horizontalalignment="right", color="0.2")
+
+    if ax is axs[0]:
+        ax.legend(loc='upper left', title=None)
+        ax.set_xlabel('')
+    elif ax is axs[1]:
+        ax.get_legend().remove()
+
+
+fig.savefig(paths.figures / "inhabited_FGKM.pdf")
