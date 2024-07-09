da21660b0511feeed708b18db0a17abb98008cf6
Martin Schlecker <martin.schlecker@mailbox.org>
2024-07-08 18:28:53 -0700

    escape any % characters

--- src/scripts/utils.py
+++ src/scripts/utils.py
@@ -24,6 +24,9 @@ def save_var_latex(key, value, datafile=paths.tex / "variables.dat"):
     if isinstance(value, float):
         value = round(value, 2)
 
+    # escape any % characters
+    value = str(value).replace("%", "\\%")
+
     dict_var[key] = value
 
     with open(file_path, "w") as f:
@@ -58,11 +61,14 @@ def timeit(func):
     Decorator for measuring function's running time.
     https://stackoverflow.com/questions/35656239/how-do-i-time-script-execution-time-in-pycharm-without-adding-code-every-time
     """
+
     def measure_time(*args, **kw):
         start_time = time.time()
         result = func(*args, **kw)
-        print("Processing time of %s(): %.2f seconds."
-              % (func.__qualname__, time.time() - start_time))
+        print(
+            "Processing time of %s(): %.2f seconds."
+            % (func.__qualname__, time.time() - start_time)
+        )
         return result
 
     return measure_time
