9fc10006dfc89bf3bd168ee10fc5183ac2d0086b
Martin Schlecker <martin.schlecker@mailbox.org>
2024-07-24 17:46:07 -0700

    show elapsed time in H:M:s

--- src/scripts/utils.py
+++ src/scripts/utils.py
@@ -58,16 +58,21 @@ def read_var_latex(key, datafile=paths.tex / "variables.dat"):
 
 def timeit(func):
     """
-    Decorator for measuring function's running time.
-    https://stackoverflow.com/questions/35656239/how-do-i-time-script-execution-time-in-pycharm-without-adding-code-every-time
+    Decorator for measuring a function's processing time.
     """
 
     def measure_time(*args, **kw):
         start_time = time.time()
         result = func(*args, **kw)
+        elapsed_time = time.time() - start_time
+
+        # Calculate hours, minutes, and seconds
+        hours, remainder = divmod(elapsed_time, 3600)
+        minutes, seconds = divmod(remainder, 60)
+
+        # Print formatted time
         print(
-            "Processing time of %s(): %.2f seconds."
-            % (func.__qualname__, time.time() - start_time)
+            f"Processing time of {func.__qualname__}(): {int(hours):02}:{int(minutes):02}:{seconds:.0f}."
         )
         return result
 
