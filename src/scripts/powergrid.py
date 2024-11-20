7aabca625d0cb5d657ef799b0777e230f40f4baf
Martin Schlecker <martin.schlecker@mailbox.org>
2024-11-20 11:04:16 -0700

    update power grid

--- src/scripts/powergrid.py
+++ src/scripts/powergrid.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import cmocean
 from bioverse import plots
 import matplotlib.ticker as ticker
+import matplotlib.gridspec as gridspec
 
 
 plotstyle.styleplots()
@@ -33,41 +34,41 @@ def plot_powergrid(grid, fig, ax, **kwargs):
         show=False,
         fig=fig,
         ax=ax,
-        zoom_factor=2,
+        zoom_factor=50,
         # zoom_factor=0,
+        smooth_sigma=.0,
         # smooth_sigma=.5,
-        smooth_sigma=0.2,
         # levels=[50, 95],
         levels=None,
         cmap=cmocean.cm.dense_r,
         **kwargs,
     )
 
-    ax.set_ylim(bottom=1e-3)
+    # ax.set_xlim(10, 1100)
+    ax.set_xlim(50, 1000)
+    ax.set_ylim(bottom=1e-3, top=1.0)
     return fig, ax
 
 
-fig, axs = plt.subplots(1, 2, figsize=[13, 4.5])
+fig = plt.figure(figsize=[13, 4.])
+gs = gridspec.GridSpec(1, 4, width_ratios=[1, 0.03, 1, 0.08], wspace=0.25)
+axs = [fig.add_subplot(gs[0, 0]), fig.add_subplot(gs[0, 2])]
+cax = fig.add_subplot(gs[0, 3])
+
 
 for i, (spt, ax) in enumerate(zip(["FGK", "M"], axs)):
-    with open(
-        paths.data / "pipeline/grid_flife_nuv_{}.dll".format(spt), "rb"
-    ) as f, open(paths.data / "pipeline/data_{}.dll".format(spt), "rb") as d:
+    with open(paths.data / "pipeline/grid_flife_nuv_{}.dll".format(spt), "rb") as f:
         grid = dill.load(f)
-        data = dill.load(d)  # do we need this?
 
-    cbar = False if i == 0 else True
-
-    fig, ax = plot_powergrid(grid, fig, ax, cbar=cbar)
+    fig, ax = plot_powergrid(grid, fig, ax, cbar=False)
 
     if i == 1:
         ax.set_ylabel("")
 
-    ax.xaxis.set_major_formatter(
-        ticker.FuncFormatter(lambda x, pos: "{:0.0f}".format(x))
-    )
+    ax.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: "{:0.0f}".format(x)))
     ax.yaxis.set_major_formatter(ticker.StrMethodFormatter("{x:.3g}"))
     ax.set_title(f"{spt}-type host stars")
 
+fig.colorbar(axs[0].collections[0], cax=cax, label="Statistical power (%)")
 
 fig.savefig(paths.figures / "powergrid.pdf")
